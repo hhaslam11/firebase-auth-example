61777467fed8de9bde35ca21efb6ff93382e3b55
hhaslam11 <kalebmarc@hotmail.com>
2020-01-25 17:37:46 -0800

    add firebase config

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
 function App() {
+
+  //firebase
+  console.log(firebaseConfig);
+
   return (
     <div className="App">
       <header className="App-header">
